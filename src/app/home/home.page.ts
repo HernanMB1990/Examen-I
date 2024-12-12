be19b8366a4241aa206de81aab68d0a07f640d54
HernanMB1990 <hernan.mosquerab@ug.edu.ec>
2024-12-12 18:06:45 -0500

    Añadiendo archivos del proyecto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,11 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
+  nombre: string = '';
+  edad: number | null = null;
+  resultado: string = '';
 
-  constructor() {}
-
+  verificarEdad() {
+    if (this.edad === null || this.nombre.trim() === '') {
+      this.resultado = 'Por favor, ingrese todos los datos.';
+    } else if (this.edad < 18) {
+      this.resultado = `${this.nombre}, eres menor de edad.`;
+    } else {
+      this.resultado = `${this.nombre}, eres mayor de edad.`;
+    }
+  }
 }
+
